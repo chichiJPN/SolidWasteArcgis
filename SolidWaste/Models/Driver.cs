0d35b9bb513a1538b9b63fc288ad550a4d6b9d3a
Junichi <Junichi>
2016-12-27 03:58:21 +0800

    12/27/2016

---
+++ SolidWaste/Models/Driver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SolidWaste.Models
+{
+    public class Driver
+    {
+        public int DriverID { get; set; }
+        public int? TruckID { get; set; }
+        public string name { get; set; }
+
+        public virtual Truck Truck { get; set; }
+
+
+    }
+}
---
+++ SolidWaste/Models/SolidWasteDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace SolidWaste.Models
+{
+    public class SolidWasteDb : DbContext
+    {
+        public SolidWasteDb()
+            : base("name=DefaultConnection")
+        { }
+
+        //public DbSet<Role> Roles { get; set; }
+        //public DbSet<User> Users { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Municipality> Municipalities { get; set; }
+        public DbSet<Address> Addresses{ get; set; }
+
+        public DbSet<Truck> Trucks { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Wastelorry> Wastelorries { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
